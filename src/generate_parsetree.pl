4f9852d9d99805a08a9e969907cebce505957a98
Nikhila6 <saini.nikhila@gmail.com>
2019-04-07 17:46:20 -0700

    Some changes
    1. Changed Variable and functions name for better readibility.
    2. Added new methods and changed the structure of the parse tree.
    3. Added data types.

--- src/generate_parsetree.pl
+++ src/generate_parsetree.pl
@@ -4,57 +4,58 @@
 
 %:-use_rendering(svgtree).
 
-/* identifier and digit values that the parse tree takes,
-they are mentioned before the program predicate as these are facts 
-that are already known to us */
-
-i(identifier(x)) --> [x].
-i(identifier(y)) --> [y].
-i(identifier(z)) --> [z].
-i(identifier(u)) --> [u].
-i(identifier(v)) --> [v].
-
-num(number(0)) --> [0].
-num(number(1)) --> [1].
-num(number(2)) --> [2].
-num(number(3)) --> [3].
-num(number(4)) --> [4].
-num(number(5)) --> [5].
-num(number(6)) --> [6].
-num(number(7)) --> [7].
-num(number(8)) --> [8].
-num(number(9)) --> [9].
-
-
 /* The program takes an input list of tokens to generate a parse tree */
- 
-program(parse_tree(X)) --> k(X).
+
+program(parse_tree(X)) --> rend_block(X).
 
 /* The parse tree begins by executing a set of statements in a block. [.]
 is mentioned as the the input program block ends with '.' */
 
-k(block(X,Y)) --> [begin], dec(X), [;], cmd(Y), [end], [.].
+rend_block(block(Y)) --> [begin], rend_command(Y), [end], [.].
 
 
-/* Checks for constants, identifiers and variables and recursively calls dec */
 
-dec(declaration(X,Y)) --> dec1(X), [;], dec(Y).
-dec(declaration(X)) --> dec1(X).
-
-dec1(const(X,Y)) --> [const], i(X), [=], num(Y).
-dec1(var(X)) --> [var], i(X).
-
-
-/* checks the syntax of identifier expressions, if-then-else, while loops 
+/* checks the syntax of identifier expressions, if-then-else, while loops
 and recursively calls cmd */
-  
-cmd(command(X,Y)) --> cmd1(X), [;], cmd(Y).
-cmd(command(X)) --> cmd1(X).
 
-cmd1(equate(X,Y)) --> i(X), [:=], expr(Y).
-cmd1(if_condition(X,Y,Z)) --> [if], bool(X), [then], cmd(Y), [else], cmd(Z), [endif].
-cmd1(while_condition(X,Y)) --> [while], bool(X), [do], cmd(Y), [endwhile].
-cmd1(X) --> k(X).
+rend_command(command(X,Y))--> rend_declaration(X), [;], rend_command(Y).
+rend_command(command(X))--> rend_declaration(X).
+
+rend_command(command(X,Y)) --> next_command(X), [;], rend_command(Y).
+rend_command(command(X)) --> next_command(X).
+	
+next_command(equate(X,Y)) --> identifier(X), [~], expr(Y).
+
+next_command(println(X)) --> [println], ['('], printVal(X), [')'].
+
+next_command(println(X, Y)) --> [println], printVal(X), [;], next_command(Y).
+
+next_command(if_condition(X,Y)) --> [if], ['('], bool(X), [')'], [:], ['{'], rend_command(Y), ['}'].
+next_command(if_else_condition(X,Y,Z)) --> [if], ['('], bool(X), [')'], [:], ['{'], rend_command(Y), ['}'], [else], ['{'], rend_command(Z), ['}'].
+next_command(if_elif_condition(V,W,X,Y,Z)) --> [if], ['('], bool(V), [')'], [:], ['{'], rend_command(W), ['}'], [elif], ['('], bool(X), [')'],[:], ['{'],rend_command(Y), ['}'], [else], ['{'], rend_command(Z), ['}'].
+
+next_command(while_condition(X,Y)) --> [while], ['('], bool(X), [')'], [:], ['{'], rend_command(Y), ['}'].
+
+next_command(while_condition(X, Y, Z)) --> [while], ['('], bool(X), [')'], [:],['{'], rend_command(Y), ['}'], [else], ['{'], rend_command(Z), ['}'].
+
+next_command(X) --> rend_block(X).
+
+printVal('"', _X, '"').
+printVal(_X).
+
+
+/* Checks for constants, identifiers and variables and recursively calls ren */
+
+% rend_declaration(declaration(X,Y)) --> next_declaration(X), [;], rend_declaration(Y).
+rend_declaration(declaration(X)) --> next_declaration(X), [;].
+
+next_declaration(const(X, Y)) --> dataType(X), identifier(X), [=], number(Y).
+next_declaration(var(X, Y)) --> dataType(X), identifier(Y).
+
+dataType(dataType(int)) --> [int].
+dataType(dataType(float)) --> [float].
+dataType(dataType(boolean)) --> [boolean].
+dataType(dataType(string)) --> [string].
 
 
 /* evaluates the boolean values- true, false, equalto and not equal to */
@@ -62,13 +63,14 @@ cmd1(X) --> k(X).
 bool(boolean(true)) --> [true].
 bool(boolean(false)) --> [false].
 bool(boolean_Expr(X,Y)) --> expr(X), [=], expr(Y).
+% bool(boolean_Expr(X,Y)) --> expr(X), [!], expr(Y), fail.
 bool(booleanl_NotExpr(X)) --> [not], bool(X).
 
 /* syntax for expressions that add, subtract, multiply and divide two terms
 with precedence rules incorporated */
 
-expr(expression(X,Y)) --> termT(X), [+], expr(Y).
-expr(expression(X,Y)) --> termT(X), [-], expr(Y).
+expr(add_expr(X,Y)) --> termT(X), [+], expr(Y).
+expr(sub_expr(X,Y)) --> termT(X), [-], expr(Y).
 expr(expression(X)) --> termT(X).
 
 termT(term_expression(X,Y)) --> termF(X), [*], termT(Y).
@@ -77,5 +79,27 @@ termT(term_expression(X)) --> termF(X).
 
 /* the expression can also take an identifier or a number */
 
-termF(X) --> num(X).
-termF(X) --> i(X).
+termF(X) --> number(X).
+termF(X) --> identifier(X).
+
+
+/* identifier and digit values that the parse tree takes,
+they are mentioned before the program predicate as these are facts
+that are already known to us */
+
+identifier(identifier(x)) --> [x].
+identifier(identifier(y)) --> [y].
+identifier(identifier(z)) --> [z].
+identifier(identifier(u)) --> [u].
+identifier(identifier(v)) --> [v].
+
+number(number(0)) --> [0].
+number(number(1)) --> [1].
+number(number(2)) --> [2].
+number(number(3)) --> [3].
+number(number(4)) --> [4].
+number(number(5)) --> [5].
+number(number(6)) --> [6].
+number(number(7)) --> [7].
+number(number(8)) --> [8].
+number(number(9)) --> [9].
