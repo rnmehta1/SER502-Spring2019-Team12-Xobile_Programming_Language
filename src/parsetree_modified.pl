8865ba6ed35fadbda2e5ec0607b278861e0b9938
Nikhila6 <saini.nikhila@gmail.com>
2019-04-20 19:12:54 -0700

    modified code for syntax tree

--- src/parsetree_modified.pl
+++ src/parsetree_modified.pl
@@ -1,98 +1,98 @@
 
-/* Generate parse tree for the DCG defined */
+/* Generate parse tree for the dcG defined */
 
 
 :-use_rendering(svgtree).
 
-/* The program takes an input list of tokens to generate a parse tree */
+/* the program takes an input list of tokens to generate a parse tree */
 
-program(parse_tree(X)) --> K(X).
+program(parse_tree(X)) --> k(X).
 
-/* The parse tree begins by executing a set of statements in a block. [.]
+/* the parse tree begins by executing a set of statements in a block. [.]
 is mentioned as the the input program block ends with '.' */
 
-K(block(Y)) --> [begin], CL(Y), [end], [.].
+k(block(Y)) --> [begin], cl(Y), [end], [.].
 
 
 
 /* checks the syntax of identifier expressions, if-then-else, while loops
 and recursively calls cmd */
 
-CL(command(X,Y))--> Fd(X), CL(Y).
-CL(command(X,Y))--> C(X), CL(Y).
-CL(command(ε)) --> ε.
+cl(command(X,Y))--> fd(X), cl(Y).
+cl(command(X,Y))--> c(X), cl(Y).
+%cl(command(ε)) --> ε.
 
 
-Fd(def(X,Y,Z)) --> [define], Name(X), ['('], Param(Y), [')'], [':'], ['{'], C(Z) ['}'].
-Name(name(X)) --> I(X).
+fd(def(X,Y,Z)) --> [define], name(X), ['('], param(Y), [')'], [':'], ['{'], c(Z) ['}'].
+name(name(X)) --> i(X).
 
-C(cond(X,Y)) --> D(X), C(Y), [';'].
-C(if_cond(X,Y,Z)) --> [if], B(X), ['{'], C(Y), ['}'],  C(Z).
-C(else_cond(W,X,Y,Z)) --> [if], B(W), ['{'], C(X), ['}'], [else], ['{'], C(Y), ['}'], C(Z).
-C(elif_cond(U,V,W,X,Y,Z)) --> [if], B(U), ['{'], C(V), ['}'], [elif], ['{'], C(W), ['}'], Elif(X), [else], ['{'], C(Y), ['}'], C(Z).
-C(while_cond(X,Y,Z)) --> [while], B(X), ['{'], C(Y), ['}'], C(Z).
-C(while_else_cond(W,X,Y,Z)) --> [while], B(W), ['{'], C(X), ['}'], [else], ['{'], C(Y), ['}'], C(Z).
-C(stop_cond(stop)) --> [stop].
-C(return_cond(return)) --> [return].
-C(fc_cond(X,Y)) --> Fc(X), C(Y).
-C(end_cond(ε)) --> ε.
+c(cond(X,Y)) --> d(X), c(Y), [';'].
+c(if_cond(X,Y,Z)) --> [if], b(X), ['{'], c(Y), ['}'],  c(Z).
+c(else_cond(W,X,Y,Z)) --> [if], b(W), ['{'], c(X), ['}'], [else], ['{'], c(Y), ['}'], c(Z).
+c(elif_cond(U,V,W,X,Y,Z)) --> [if], b(U), ['{'], c(V), ['}'], [elif], ['{'], c(W), ['}'], elif(X), [else], ['{'], c(Y), ['}'], c(Z).
+c(while_cond(X,Y,Z)) --> [while], b(X), ['{'], c(Y), ['}'], c(Z).
+c(while_else_cond(W,X,Y,Z)) --> [while], b(W), ['{'], c(X), ['}'], [else], ['{'], c(Y), ['}'], c(Z).
+c(stop_cond(stop)) --> [stop].
+c(return_cond(return)) --> [return].
+c(fc_cond(X,Y)) --> fc(X), c(Y).
+%c(end_cond(ε)) --> ε.
 
 
-D(bool(X)) --> [boolean], I(X), ['='], B1(Y), [';'].
-D(data_equate(X,Y,Z)) --> Dt(X), I(Y), ['='], E(Z), [';'].
-D(date_init(X,Y)) --> Dt(X), I(Y), [';'].
-D(equate(X,Y)) --> I(X), ['='], E(Y), [';'].
+d(bool(X)) --> [boolean], i(X), ['='], b1(Y), [';'].
+d(data_equate(X,Y,Z)) --> dt(X), i(Y), ['='], e(Z), [';'].
+d(date_init(X,Y)) --> dt(X), i(Y), [';'].
+d(equate(X,Y)) --> i(X), ['='], e(Y), [';'].
 
-Dt(datatype(int)) --> [int].
-Dt(datatype(float)) --> [float].
-Dt(datatype(string)) --> [string]. 
-Dt(datatype(boolean)) -->[boolean].
-Bl(bool(true)) --> [true].
-Bl(bool(false)) --> [false].
+dt(datatype(int)) --> [int].
+dt(datatype(float)) --> [float].
+dt(datatype(string)) --> [string]. 
+dt(datatype(boolean)) -->[boolean].
+b1(bool(true)) --> [true].
+b1(bool(false)) --> [false].
 
-Elif(else_if(X,Y)) --> [elif], ['{'], C(X), ['}'], Elif(Y).
-Elif(stop(ε)) --> ε.
+elif(else_if(X,Y)) --> [elif], ['{'], c(X), ['}'], elif(Y).
+%elif(stop(ε)) --> ε.
 
-Fc(name_param(X,Y)) --> Name(X), ['('], Param(Y), [')'].
-Param(param(X,Y)) --> I(X), [','], Param(Y).
-Param(id(X)) --> I(X).
-Param(stop(ε)) --> ε.
+fc(name_param(X,Y)) --> name(X), ['('], param(Y), [')'].
+param(param(X,Y)) --> i(X), [','], param(Y).
+param(id(X)) --> i(X).
+%param(stop(ε)) --> ε.
 
-B(boolean(true)) --> [true].
-B(boolean(false)) --> [false].
-B(expr_equate(X,Y)) --> E(X), ['~'], E(Y).
-B(not(X)) --> [not], B(X).
-B(not(X)) --> ['!'], B(X).
-B(zero(X)) --> ['0'].
-B(one(X)) --> ['1'].
+b(boolean(true)) --> [true].
+b(boolean(false)) --> [false].
+b(expr_equate(X,Y)) --> e(X), ['~'], e(Y).
+b(not(X)) --> [not], b(X).
+b(not(X)) --> ['!'], b(X).
+b(zero(X)) --> ['0'].
+b(one(X)) --> ['1'].
 
-E(expr_add(X,Y)) --> T(X), ['+'], E(Y).
-E(expr_sub(X,Y)) --> T(X), ['-'], E(Y).
-E(term(X)) --> T(X).
+e(expr_add(X,Y)) --> t(X), ['+'], e(Y).
+e(expr_sub(X,Y)) --> t(X), ['-'], e(Y).
+e(term(X)) --> t(X).
 
-T(term_expression(X,Y)) --> ['('], E(X), [')'], T(Y).
-T(term_mult(X,Y)) --> Nt(X), ['*'], T(Y).
-T(term_div(X,Y)) --> Nt(X), ['/'], T(Y).
-T(terminal(X)) --> Nt(X).
+t(term_expression(X,Y)) --> ['('], e(X), [')'], t(Y).
+t(term_mult(X,Y)) --> nt(X), ['*'], t(Y).
+t(term_div(X,Y)) --> nt(X), ['/'], t(Y).
+t(terminal(X)) --> nt(X).
 
-Nt(terminal_id(X)) --> I(X).
-Nt(terminal_num(X)) --> N(X).
+nt(terminal_id(X)) --> i(X).
+nt(terminal_num(X)) --> n(X).
 
-I(X) --> [X].
+i(X) --> [X].
 
-N(num(X,Y)) --> D(X), [','], N(Y).
-N(num(ε)) --> ε.
+n(num(X,Y)) --> d(X), [','], n(Y).
+%n(num(ε)) --> ε.
 
-D(digit(0)) --> ['0'].
-D(digit(1)) --> ['1'].
-D(digit(2)) --> ['2'].
-D(digit(3)) --> ['3'].
-D(digit(4)) --> ['4'].
-D(digit(5)) --> ['5'].
-D(digit(6)) --> ['6'].
-D(digit(7)) --> ['7'].
-D(digit(8)) --> ['8'].
-D(digit(9)) --> ['9'].
+d(digit(0)) --> ['0'].
+d(digit(1)) --> ['1'].
+d(digit(2)) --> ['2'].
+d(digit(3)) --> ['3'].
+d(digit(4)) --> ['4'].
+d(digit(5)) --> ['5'].
+d(digit(6)) --> ['6'].
+d(digit(7)) --> ['7'].
+d(digit(8)) --> ['8'].
+d(digit(9)) --> ['9'].
 
 
 
