a1ea294572930aecff8a85769da266431c3915a6
vsinghal3737 <42525531+vsinghal3737@users.noreply.github.com>
2019-04-27 12:57:38 -0700

    Semantic Part

---
+++ src/semanticMain.pl
@@ -0,0 +1,97 @@
+%:-use_rendering(svgtree).
+
+lookup(Var, [[_Type, Var, Val]| _], Val).
+lookup(Var, [_ | T], Val):-
+    lookup(Var, T, Val).
+
+update([Type, Var], [[Type, Var, _] | T], Val, [[Type, Var, Val] | T]).
+update([Type, Var], [H | T], Val, [H | Env]):-
+    update([Type, Var], T, Val, Env).
+
+add_Env([Type, Var, Val], Env, Result):-
+    Temp = [[Type, Var, Val]],
+    append(Env, Temp, Result).
+
+eval_program(start(X), Op):-
+    eval_block(X, [], Op),
+    lookup(_Z, Op, _New).
+
+eval_block(block(), _Env, _OpEnv):-
+    write('empty Program').
+eval_block(block(X), Env, OpEnv):-
+    eval_dec(X, NewOp),
+    append(Env, NewOp, OpEnv).
+
+eval_dec(declaration(X, Y), OpEnv):-
+    eval_next_dec(X, NewOp1),
+    eval_dec(Y, NewOp2),
+    append(NewOp1,NewOp2, OpEnv).
+eval_dec(declaration(X), OpEnv):-
+    eval_next_dec(X, NewOp),
+    OpEnv = NewOp.
+
+eval_next_dec(boolean(X), Env) :-
+    L = [],
+    add_Env([boolean, X, nil], L, Env).
+eval_next_dec(integer(X), Env) :-
+    L = [],
+    add_Env([int, X, nil], L, Env).
+
+
+/*Work in Progress*/
+/*eval_cmd(command(X,Y)) --> eval_next_cmd(X), eval_cmd(Y).
+eval_cmd(command(X)) --> eval_next_cmd(X).
+
+eval_next_cmd(assign(X, Y)) --> identifier(X), [=], eval_expr(Y), [;].
+eval_next_cmd(print(X)) --> [print], element(X), [;].
+eval_next_cmd(if(X, Y, Z)) --> [if], eval_bool_expr(X), ['{'], eval_cmd(Y), ['}'], [else], ['{'], eval_cmd(Z), ['}'].
+eval_next_cmd(while(X, Y)) --> [while], eval_bool_expr(X), ['{'], eval_cmd(Y), ['}'].
+eval_next_cmd(new_block(X)) --> rend_block(X).
+
+element(printExpr(X)) --> Z is eval_expr(X), write(Z).
+element(printExpr(X)) --> Z is eval_bool_expr(X), write(Z).
+
+eval_bool_expr(truebool(true)) --> [true].
+eval_bool_expr(truebool(false)) --> [false].
+eval_bool_expr(bool_exp(X, Y)) --> eval_expr(X), [~], eval_expr(Y).
+eval_bool_expr(bool_not(X)) --> [not], eval_bool_expr(X).
+eval_bool_expr(bool_not(X)) --> [!], eval_bool_expr(X).
+
+eval_expr(add_expr(X, Y)) --> eval_term(X), [+], eval_expr(Y).
+eval_expr(sub_expr(X, Y)) --> eval_term(X), [-], eval_expr(Y).
+eval_expr(X) --> eval_term(X).
+
+eval_term(eval_term(X, Y)) --> ['('], eval_expr(X), [')'], eval_term(Y).
+eval_term(mul_term(X, Y)) --> eval_new_term(X), [*], eval_term(Y).
+eval_term(div_term(X, Y)) --> number(X), [/], eval_term(Y).
+eval_term(num(X)) --> eval_new_term(X).
+
+eval_new_term(new_term_val(X)) --> identifier(X); number(X).
+
+identifier(u) --> [u].
+identifier(v) --> [v].
+identifier(x) --> [x].
+identifier(y) --> [y].
+identifier(z) --> [z].
+
+
+
+% number(num(X, Y)) --> digit(X), number(Y).
+% number(num()) --> []
+number(num(X, Y)) --> digit(X),number(Y).
+number(num(X)) --> digit(X).
+
+digit(0) --> [0].
+digit(1) --> [1].
+digit(2) --> [2].
+digit(3) --> [3].
+digit(4) --> [4].
+digit(5) --> [5].
+digit(6) --> [6].
+digit(7) --> [7].
+digit(8) --> [8].
+digit(9) --> [9].
+
+
+% L = [begin, boolean, x, ';', int, y, ';', int, z, ;, z, =, 0, ';', if, x, ~, x, '{', print, x, ';', '}', else, '{', z, '=', 5, ';', x, =, y, +, 2, ';', '}', while, not, x, ~, z, '{', z, =, z, +, 2, ';', print, x,~,x,';', '}', end,'.'], program(G, L, []).
+*/
