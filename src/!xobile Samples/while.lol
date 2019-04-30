6af2b9120f689cdd9b55ff4e82a1e7b0fa6e1843
vsinghal3737 <42525531+vsinghal3737@users.noreply.github.com>
2019-04-30 04:33:48 -0700

    .

---
+++ src/!xobile Samples/while.lol
@@ -0,0 +1,12 @@
+begin
+int x;
+int y;
+x = 4;
+y = 0;
+while not x ~ y
+{
+	print x;
+	x = y + 1;
+}
+end
+.
--- src/mainFile.py
+++ src/mainFile.py
@@ -8,7 +8,6 @@ LinesOfTokens = (Tokens.Tokenization("!xobile Samples//{}".format(fileName)))
 
 with open('!xobile Samples//{}tokens'.format(fileName[:-3]), 'w') as tokens:
     print(LinesOfTokens, file=tokens, flush=True)
-print(LinesOfTokens)
 
 with open('!xobile Samples//{}tokenstreams'.format(fileName[:-3]), 'w') as tokens:
     for i in LinesOfTokens:
